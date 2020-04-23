588feead1cefbb51d20d8fec09a2ae39a6cd5f58
Natawat Sirichaitayakul <natawatbank@gmail.com>
2020-04-23 09:21:44 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+    int x = 10;
+    cout << x << endl;
+    cout << x;
+    cout << " ";
+    cout << endl;
+}
