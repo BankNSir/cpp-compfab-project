7a3a16da11469e611f7f76280b7c7967c9cabb22
Natawat Sirichaitayakul <natawatbank@gmail.com>
2020-04-26 11:16:15 +0700

    print exit status in load/write obj example

--- examples/tiny_obj_loader_example.cpp
+++ examples/tiny_obj_loader_example.cpp
@@ -6,22 +6,33 @@
 
 // g++ tiny_obj_loader_example.cpp tiny_obj_loader.cc obj_writer.cc
 
+// this example download cube.obj file in current directory and write an obj file named output_cube.obj
 int main()
 {
+    // load obj file and write output obj file
     std::string inputfile = "cube.obj";
-    // load file and display to meshlab
+
     tinyobj::attrib_t attribs;
     std::vector<tinyobj::material_t> materials;
     std::vector<tinyobj::shape_t> shapes;
-
     std::string warn;
     std::string err;
-    bool ret = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
+
+    bool load_ret = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
     
     const std::string outputfile = "output_cube.obj";
 
     bool write_ret = WriteObj(outputfile, attribs, shapes, materials, false);
-    std::cout << write_ret;
+    std::string write_ret_text;
+
+    if(write_ret == true){
+        write_ret_text = "Successfully write obj file";
+    }
+    else{
+        write_ret_text = "Failed to write obj file";
+    }
+
+    std::cout << "exit status: " << write_ret_text << std::endl;
     
     return 0;
 }
