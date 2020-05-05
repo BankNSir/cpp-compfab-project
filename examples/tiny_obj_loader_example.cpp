38e4e77c5169f5779a663f96cc20bfb5693bb642
Natawat Sirichaitayakul <natawatbank@gmail.com>
2020-05-05 12:13:19 +0700

    display the loaded vertices

--- examples/tiny_obj_loader_example.cpp
+++ examples/tiny_obj_loader_example.cpp
@@ -3,9 +3,49 @@
 #include <vector>
 #include <iostream>
 
+void displayVertices(tinyobj::attrib_t &t_attribs) {
+
+    // for accessing vertex
+    std::vector<tinyobj::real_t> vertices_holder = t_attribs.vertices;
+    int index = 0;
+    for (auto &val : vertices_holder) {
+
+        std::cout << val << " ";
+
+        if ( index % 3 == 2 ) {
+            std::cout << std::endl;
+        }
+
+        index++;
+    }
+    // std::cout << t_attribs << std::endl;
+    int nVertices = index / 3;
+    std::cout << "Number of Points: " << nVertices << std::endl;
+    std::cout << "This is after debug" << std::endl;
+}
+
+void displayNormals(tinyobj::attrib_t &t_attribs) {
+
+    // for accessing vertex
+    std::vector<tinyobj::real_t> vertices_holder = t_attribs.normals;
+    int index = 0;
+    for (auto &val : vertices_holder) {
+
+        std::cout << val << " ";
+
+        if ( index % 3 == 2 ) {
+            std::cout << std::endl;
+        }
+
+        index++;
+    }
+    // std::cout << t_attribs << std::endl;
+    int nNormals = index / 3;
+    std::cout << "Number of point: " << nNormals << std::endl;
+    std::cout << "This is after debug" << std::endl;
+}
 
 // g++ tiny_obj_loader_example.cpp tiny_obj_loader.cc obj_writer.cc
-
 // this example download cube.obj file in current directory and write an obj file named output_cube.obj
 int main()
 {
@@ -19,7 +59,12 @@ int main()
     std::string err;
 
     bool load_ret = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
-    
+
+    // Debug
+    displayVertices(attribs);
+    displayNormals(attribs);
+    std::cout << "endDebug" << std::endl;
+
     const std::string outputfile = "output_cube.obj";
 
     bool write_ret = WriteObj(outputfile, attribs, shapes, materials, false);
--- main.cpp
+++ main.cpp
@@ -5,15 +5,23 @@
 #include "Eigen/Dense"       // for linear algebra operation
 // #include "tiny_obj_loader.h"    // load and save obj files
 
+struct Particle
+{
+    // velocity waiting for eigen matrix
+    // position
+    float mass;
+};
 
 int main()
 {
 
-    Eigen::MatrixXd test_matrix(2, 2);
-    test_matrix(0, 0) = 3;
-    test_matrix(0, 1) = 2;
-    test_matrix(1, 0) = 2;
-    test_matrix(1, 1) = 3;
-    std::cout << test_matrix << std::endl;
+    // Eigen::MatrixXd test_matrix(2, 2);
+    // test_matrix(0, 0) = 3;
+    // test_matrix(0, 1) = 2;
+    // test_matrix(1, 0) = 2;
+    // test_matrix(1, 1) = 3;
+    // std::cout << test_matrix << std::endl;
 
+    Eigen::Vector2d test_vector(2, 2);
+    std::cout << test_vector << std::endl;
 }
