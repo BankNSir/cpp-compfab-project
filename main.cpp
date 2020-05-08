77b8ab497beab6da744b7cc90b8ed61ee92eba8c
Natawat Sirichaitayakul <natawatbank@gmail.com>
2020-05-08 11:03:33 +0700

    outline main.cpp and create particle struct

--- main.cpp
+++ main.cpp
@@ -5,12 +5,27 @@
 #include "Eigen/Dense"       // for linear algebra operation
 // #include "tiny_obj_loader.h"    // load and save obj files
 
+// using namespace Eigen;
+// using Vector2d = Vec2;
+using Vec2 = Eigen::Vector2d;
+using Mat2 = Eigen::Matrix2d;
+// using Eigen::Matrix2d = Mat2;
+
 struct Particle
 {
-    // velocity waiting for eigen matrix
-    // position
+    // velocity waiting for eigen matrix vector 2x2
+    Vec2 v;
+    // position vector 2x2
+    Vec2 x;
+    // deformation gradient matrix 2x2
+    Mat2 F;
+    // APIC coefficient matrix 2x2
+    Mat2 C;
     float mass;
 };
+
+// need to create grid struct here
+
 
 int main()
 {
@@ -24,4 +39,23 @@ int main()
 
     Eigen::Vector2d test_vector(2, 2);
     std::cout << test_vector << std::endl;
+
+    // main program
+
+    // I need Particle and Grid struct first
+    // So i need to use eigen for vector and matrix
+    
+    // initialize() 
+
+    // sendinfo from particle to grid
+    // particleToGrid()
+
+    // update grid info
+    // gridUpdate()
+
+    // sendinfo from grid to particle
+    // gridToParticle()
+    
+    // write an objfile by particle vertices
+    // visualize() 
 }
