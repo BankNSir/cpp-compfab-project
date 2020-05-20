63d6de6d4c6e67d0b05260b2464c75b255eea33f
Natawat Sirichaitayakul <natawatbank@gmail.com>
2020-05-20 23:11:46 +0700

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,11 @@
 #include "Eigen/Dense"       // for linear algebra operation  compile: -I .
 #include "myincludes/tiny_obj_loader.h"    // load and save obj files
 
-// using namespace Eigen;
-// using Vector2d = Vec2;
+
 using Vec2 = Eigen::Vector2d;
 using Vec3 = Eigen::Vector3d;
 using Mat2 = Eigen::Matrix2d;
-// using Eigen::Matrix2d = Mat2;
+
 
 // number of grid boxes
 const int N_GRID_BOX = 16;
@@ -35,12 +34,9 @@ const auto hardening = 10.0;
 const auto E = 1e4;          // Young's Modulus
 const auto nu = 0.2;         // Poisson ratio
 
-// Initial Lamé parameters
+// starting lame parameters
 const double mu_0 = E / (2 * (1 + nu));
 const double lambda_0 = E * nu / ((1+nu) * (1 - 2 * nu));
-
-// plasticity -> off for quicker calculation
-const bool PLASTICITY = false;
 
 struct Particle
 {
@@ -59,7 +55,7 @@ struct Particle
 
     Particle() = default;
     Particle(Vec2 t_position, Vec2 t_velocity = Vec2(0, 0), double J = 1)
-    : position(t_position), velocity(t_velocity) // if this bug initialize vector by Eigen format v << 1, 2
+    : position(t_position), velocity(t_velocity)
     {
         // Eigen initialize format
         F << 1, 1, 1, 1;
@@ -67,10 +63,8 @@ struct Particle
     }
 };
 
-// need to create grid struct here
-// with grid struct I need to have mass and velocity
 
-// Grid Coord(v, m)
+// Grid Coord(vx, vy, m)
 Vec3 GRID[N_GRID_BOX + 1][N_GRID_BOX + 1];
 
 // set particle data pass by reference -> I want to change particles without returning it
@@ -87,9 +81,6 @@ void Initialize(std::vector<Particle> &particles, const std::vector<Vec2> &posit
 // with each particle spacing is particle_space
 std::vector<Vec2> CreateBox(Vec2 bottom_left, Vec2 top_right, double particle_space)
 {   
-    // 1) calc v2-v1 --> look Eigen
-    // 2) nParticle_create = floor((v2-v1)/particle_space) --> Vec2/constant Eigen
-    // backup: Vec2 nParticle_in_box = Vec2((top_right - bottom_left)/particle_space)
     
     Vec2 nParticle_in_box = Vec2((top_right - bottom_left)/particle_space);
     int xParticle_in_box = floor(nParticle_in_box(0));
@@ -97,8 +88,6 @@ std::vector<Vec2> CreateBox(Vec2 bottom_left, Vec2 top_right, double particle_sp
 
     std::vector<Vec2> box_Coords;
 
-    // 3) v1 + i*interval from i = 0 to i = nParticle_create use normal for loop
-    // <= is correct
     for (int i = 0; i <= xParticle_in_box; i++)
     {
         for (int j = 0; j <= yParticle_in_box; j++)
@@ -109,76 +98,29 @@ std::vector<Vec2> CreateBox(Vec2 bottom_left, Vec2 top_right, double particle_sp
     return box_Coords;
 }
 
-// pos coord in [0, 1)
-// find which grid are in this particle kernel 
-std::vector<Vec2> GetGridCoord(Particle const &particle)
-{
-    // calculate pos x and pos y separately
-    // transform to grid coords
-    double dPosX = particle.position[0] * N_GRID_BOX;
-    double dPosY = particle.position[1] * N_GRID_BOX;
-
-    // minus 0.5
-    // floor to get result
-    int dBaseCoordX = floor(dPosX - 0.5);
-    int dBaseCoordY = floor(dPosY - 0.5);
-
-    std::vector<Vec2> surroundGridCoords;
-
-    // create 3x3 by +1 +2 and push vec2(x, y) into vector<Vec2>
-    for (int indexX = 0; indexX < 3; indexX++)
-    {
-        for (int indexY = 0; indexY < 3; indexY++)
-        {
-            int coordX = dBaseCoordX + indexX;
-            int coordY = dBaseCoordY + indexY;
-
-            // hack for boundary -1, N_GRID_BOX + 1
-            if (coordX == -1)
-            {
-                coordX = 0; 
-            }
-            else if (coordX == N_GRID_BOX + 1)
-            {
-                coordX = N_GRID_BOX;
-            }
-            if (coordY == -1)
-            {
-                coordY = 0;
-            }
-            else if (coordY == N_GRID_BOX + 1)
-            {
-                coordY = N_GRID_BOX;
-            }
-
-            surroundGridCoords.push_back(Vec2(coordX, coordY));
-        }
-    }
-    
-    return surroundGridCoords;
-}
-
 // element-wise power
 Vec2 PowVec2(Vec2 vec, int exponent) 
 {
     return Vec2(pow(vec[0], exponent), pow(vec[1], exponent));
 }
 
-double DetMat2(Mat2 mat) 
-{
-    return mat(0, 0)*mat(1, 1) - mat(1, 0)*mat(0, 1);
-}
-
+// element-wise floor
 Eigen::Vector2i FloorVec2(Vec2 vec) 
 {
     return Eigen::Vector2i(static_cast<int>(vec[0]), static_cast<int>(vec[1]));
 }
 
+// det of matrix 2x2
+double DetMat2(Mat2 mat) 
+{
+    return mat(0, 0)*mat(1, 1) - mat(1, 0)*mat(0, 1);
+}
+
+
 // This function send info from particle to grid
 void ParticleToGrid(const std::vector<Particle> &particles)
 {
-    for (auto &p: particles) {
-        
+    for (auto &p: particles) {  
         // Leftmost coord of this particle kernel grid
         Eigen::Vector2i baseCoords = FloorVec2(p.position * N_GRID_BOX - Vec2(0.5, 0.5));
         Vec2 baseCoordToParticleDist = p.position * N_GRID_BOX - baseCoords.cast<double>();
@@ -246,12 +188,10 @@ void UpdateGrid() {
                 double boundary = 0.05;
                 double nodeCoordsX = static_cast<double>(i) / N_GRID_BOX; 
                 double nodeCoordsY = static_cast<double>(j) / N_GRID_BOX;
-                // printf("%.2f %.2f  ", nodeCoordsX, nodeCoordsY);
 
                 // sticky
                 if (nodeCoordsX < boundary || nodeCoordsX > 1-boundary || nodeCoordsY > 1-boundary) {
                     grid = Vec3(0, 0, 0);
-                    // std::cout << "STICKY" << std::endl;
                 }            
 
                 // seperate
@@ -312,7 +252,7 @@ void GridToParticle(std::vector<Particle> &particles) {
 
         double oldJ = DetMat2(F);
 
-        // // snow pasticity (not yet)
+        // // snow pasticity (comment this out when all simulation is correct)
         // Vec2 sigVec;
         // Mat2 svd_u, svd_v;
         // Eigen::JacobiSVD<Eigen::MatrixXd> svd(F, Eigen::ComputeThinU | Eigen::ComputeThinV);
@@ -350,9 +290,7 @@ void ShowGrid(const Vec3 grid[][N_GRID_BOX+1]) {
 }
 
 void ShowParticle(const std::vector<Particle> &particles, int displayWidth = 12) {
-    // for (auto &p : particles) {
-    //     printf("%.3f %.3f | ", p.position.x(), p.position.x());
-    // }
+
     for (int parIndex = 0; parIndex < particles.size(); parIndex++) {
         auto &p = particles[parIndex];
         printf("%.3f %.3f | ", p.position.x(), p.position.y());
@@ -366,7 +304,6 @@ void ShowParticle(const std::vector<Particle> &particles, int displayWidth = 12)
 void AdvanceSimulation(std::vector<Particle> &particles) {
     // Reset grid
     std::memset(GRID, 0, sizeof(GRID));
-    // ShowGrid(GRID);
 
     ParticleToGrid(particles);
     UpdateGrid();
@@ -379,8 +316,8 @@ int main()
     
     // create box objects
     // boxCoord, positoin must be in range [0, 1)
-    std::vector<Vec2> boxCoords = CreateBox(Vec2(0.5, 0.5), Vec2(0.7, 0.7), 0.01);
-    std::vector<Vec2> box2Coords = CreateBox(Vec2(0.4, 0.4), Vec2(0.6, 0.6), 0.01);
+    std::vector<Vec2> boxCoords = CreateBox(Vec2(0.5, 0.5), Vec2(0.6, 0.6), 0.01);
+    std::vector<Vec2> box2Coords = CreateBox(Vec2(0.45, 0.4), Vec2(0.55, 0.55), 0.01);
 
 
     // get box particles info
@@ -403,31 +340,7 @@ int main()
     std::cout << "After Update" << std::endl;
     ShowParticle(particles, 11);
     
-    // write an objfile by particle vertices
     // visualize() : TODO
-
 
     std::cout << "End of program" << std::endl;
 }
-
-
-// Eigen::Vector2d test_vector(2, 2);
-// std::cout << test_vector << std::endl;
-
-// Eigen::MatrixXd test_matrix(2, 2);
-    // test_matrix(0, 0) = 3;
-    // test_matrix(0, 1) = 2;
-    // test_matrix(1, 0) = 2;
-    // test_matrix(1, 1) = 3;
-    // std::cout << test_matrix << std::endl;
-
-    //DEBUG
-    // Vec2 a(1.0);
-    // std::cout << temp * 10 << std::endl;
-    // std::cout << typeid(temp(0)).name() << std::endl;
-    // std::cout << temp[0] << " " << temp[1] << std::endl << temp(0) << " " << temp(1) << " " << temp(0, 0) << std::endl;
-    // Mat2 a(2, 2);
-    // a << 1, 1, 1, 1;
-    // Vec2 a(1.0, 2.0);
-    // std::cout << a << std::endl;
-    // std::cout << a.x() << " " << a.y() << std::endl;
